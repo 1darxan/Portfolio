59481f1f72978c781193386026ef4f1d4842c7f9
Darxan <darxantursunov@gmail.com>
2023-10-29 09:16:13 +0500

    Portfolio

---
+++ main.js
@@ -0,0 +1,11 @@
+let typed = new Typed('.type_anim',{
+    strings:['Frontent Developer','Html5', 'Css3', 'Git' ,'GitHub', 'JavaScript','VueJs'],
+    typeSpeed:100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+})
+
+
+
+
